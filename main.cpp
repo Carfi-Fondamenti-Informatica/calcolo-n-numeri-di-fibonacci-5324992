c38729740078fea2517d4be2fd769cfedbff14fb
5324992 <91890608+5324992@users.noreply.github.com>
2021-11-13 20:54:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+   int n=0,a=1,b=1,somma=a+b;
+   cin>>n
+   
+   cout<<a<<endl<<b<<endl;
+   for (i=1;i<=n;i++) {
+      somma=a+b;
+      cout<<somma<<endl;
+      a=b;
+      b=somma;
+   }
    return 0;
 }
