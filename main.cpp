c773dae4099130850afca842bd512848d69e8159
5324992 <91890608+5324992@users.noreply.github.com>
2021-11-14 15:49:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   int a=1,b=1;
- 
-   cout<<a<<endl<<b<<endl;
-   for (i=1;i<=n;i++) {
-      somma=a+b;
-      cout<<somma<<endl;
-      a=b;
-      b=somma;
+int main()
+{
+   int n,i=1,b=1,a=0,c;
+   cin>>n;
+   
+   if(n>=2)
+   {
+      cout<<b<<endl;
+      for(i=2;i<=n;i++)
+      {
+         
+         c=a+b;
+         cout<<c<<endl;
+         a=b;
+         b=c;
+      
+         }
+      
    }
+   else
+   {
+      cout<<"errore"<<endl;
+   }   
    return 0;
 }
